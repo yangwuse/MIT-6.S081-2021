ad05b20931fd3be9c54de484f553db168792b467
yw <yangwuse@mail.ustc.edu.cn>
2021-10-24 11:33:22 +0800

    Delete execv.c

--- LEC1/examples/execv.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>  /* execv() */                                                          
-#include <stdlib.h>  /* exit() */
-
-int main()
-{
-    char *args[] = {"echo", "hello", "world", 0 };
-    execv("/bin/echo", args);
-    exit(0);
-}
