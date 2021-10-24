9b29f3ff18899aa1ec1fb88b90afdba7f6c2b0d7
yw <yangwuse@mail.ustc.edu.cn>
2021-10-24 11:49:50 +0800

    Create fork.c

---
+++ LEC1/examples/fork.c
@@ -0,0 +1,15 @@
+// fork.c: create a new process
+#include <unistd.h>  /* fork() */
+#include <stdio.h>   /* printf() */                                                         
+#include <stdlib.h>  /* exit() */
+
+int main()
+{
+    int pid = fork();
+    printf("fork() returned %d\n", pid);
+    if (pid == 0)  
+        printf("child\n");
+    else
+        printf("parent\n");
+    exit(0);
+}
